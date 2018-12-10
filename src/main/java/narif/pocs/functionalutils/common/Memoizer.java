093c73f5b543393927a0c90169849998bd0fbe5a
NajeebArif <NajeebArif786@outlook.com>
2018-12-10 23:17:05 +0530

    Adding Memoization

---
+++ src/main/java/narif/pocs/functionalutils/common/Memoizer.java
@@ -0,0 +1,22 @@
+package narif.pocs.functionalutils.common;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import narif.pocs.functionalutils.functions.Function;
+
+public class Memoizer<T, U> {
+	
+	private final Map<T, U> cache = new ConcurrentHashMap<>();
+	
+	private Memoizer() {}
+	
+	public static <T, U> Function<T, U> memoize(Function<T, U> function){
+		return new Memoizer<T, U>().doMemoize(function);
+	}
+
+	private Function<T, U> doMemoize(Function<T, U> function) {
+		return input -> cache.computeIfAbsent(input, function::apply);
+	}
+
+}
